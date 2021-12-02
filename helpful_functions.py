bd5e2a334211726158b030a4b0e5a415348abb0d
0x00 <89746056+j0912345@users.noreply.github.com>
2021-12-01 16:22:04 -0800

    pgsr/pgsr file extractor

---
+++ helpful_functions.py
@@ -0,0 +1,10 @@
+def convert_data_to_uint(var_name, offset_to_read_from, opened_file_name):
+    import numpy as np
+    opened_file_name.seek(offset_to_read_from)
+    var_name=opened_file_name.read(4)
+    var_name=np.frombuffer(var_name, dtype=np.uint32)
+    var_name=int(var_name)
+    return(var_name)
+
+def go_to_offset_from_current_offset(offset, opened_file_name):
+    opened_file_name.seek(opened_file_name.tell()+offset)
